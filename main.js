5e0e52306cfe26776a2abb81ec8acfb1452bf4ad
Joe Nelson <cred+github@begriffs.com>
2014-03-06 11:53:17 -0800

    Load yaml api definition

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 requirejs.config({
   shim: {
-    'angular': { exports: 'angular' }
+    'angular': { exports: 'angular' },
+    'jsyaml': { exports: 'jsyaml' }
   },
   paths: {
     propertyParser: 'components/requirejs-plugins/src/propertyParser',
     domReady: 'components/requirejs-domready/domReady',
-    angular: 'components/angular/angular.min'
+    angular: 'components/angular/angular.min',
+    jsyml: 'components/js-yaml/js-yaml'
   }
 });
 
 require(
   [
     'angular',
+    'jsyml',
     'domReady!'
   ],
-  function (angular, jsontemplate) {
+  function (angular, jsyml) {
     angular.module('ramlview', []);
 
     angular.module('ramlview').
       controller('RamlViewCtrl', function($scope, $http) {
-
+        $http.get('bountysource.raml').then(function (response) {
+          $scope.api = jsyml.load(response.data);
+        });
       });
 
     angular.bootstrap(document, ['ramlview']);
