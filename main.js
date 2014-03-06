0127734cc50158daa06159aaf0f006cbd2df23f1
Joe Nelson <cred+github@begriffs.com>
2014-03-06 12:04:37 -0800

    Parse raml routes

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 requirejs.config({
   shim: {
     'angular': { exports: 'angular' },
-    'jsyaml': { exports: 'jsyaml' }
+    'jsyaml': { exports: 'jsyaml' },
+    'underscore': { exports: '_' }
   },
   paths: {
     propertyParser: 'components/requirejs-plugins/src/propertyParser',
     domReady: 'components/requirejs-domready/domReady',
     angular: 'components/angular/angular.min',
-    jsyml: 'components/js-yaml/js-yaml'
+    jsyml: 'components/js-yaml/js-yaml',
+    underscore: 'components/underscore/underscore'
   }
 });
 
@@ -15,15 +17,22 @@ require(
   [
     'angular',
     'jsyml',
+    'underscore',
     'domReady!'
   ],
-  function (angular, jsyml) {
+  function (angular, jsyml, _) {
     angular.module('ramlview', []);
 
     angular.module('ramlview').
       controller('RamlViewCtrl', function($scope, $http) {
         $http.get('bountysource.raml').then(function (response) {
           $scope.api = jsyml.load(response.data);
+          $scope.routes = _.reduce(
+            _.filter(_.keys($scope.api), function (k) { return k[0] === '/'; }),
+            function (snowball, flake) {
+              snowball[flake] = $scope.api[flake];
+              return snowball;
+            }, {})
         });
       });
 
